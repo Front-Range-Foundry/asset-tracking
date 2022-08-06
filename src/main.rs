438293e4e63a74405f2320e239c340eca82596e8
jackdixonryan <jack.dixon.ryan@gmail.com>
2022-08-06 08:25:42 -0600

    Delete works.
    - After much toil, can now delete assets.
    - Updating is... harder.

--- src/main.rs
+++ src/main.rs
@@ -48,11 +48,13 @@ impl Session {
                         self.create_asset()?;
                     },
                     1 => {
-                        self.lookup_asset()?;
+                        self.lookup_asset(true);
                     },
                     2 => {
+                        self.update_asset();
                     },
                     3 => {
+                        self.delete_asset();
                     },
                     4 => {
                         self.view_assets();
@@ -96,31 +98,77 @@ impl Session {
         table.printstd();
     }
 
-    fn lookup_asset(&self) -> std::io::Result<()> {
+    fn lookup_asset(&self, should_render: bool) -> Option<Asset> {
         let assets = &self.asset_library.dir;
         let input: String = Input::new()
             .with_prompt("Enter asset uuid")
-            .interact_text()?;
+            .interact_text()
+            .expect("Invalid io entry.");
 
         match Uuid::parse_str(&input) {
             Ok(uuid) => {
                 match assets.iter().position(|r| r.id == uuid) {
                     Some(index) => {
-                        let mut table = Table::new();
-                        table.add_row(row!["ID", "NAME"]);
-                        table.add_row(row![assets[index].id, assets[index].name]);
-                        table.printstd();
+                        if should_render {  
+                            let mut table = Table::new();
+                            table.add_row(row!["ID", "NAME"]);
+                            table.add_row(row![assets[index].id, assets[index].name]);
+                            table.printstd();
+                        }
+                        let asset = &assets[index];
+                        Some(asset)
                     },
                     None => {
                         println!("No asset with UUID {} could be located.", uuid);
+                        None
                     }
                 };
             },
-            Err(e)  => {
+            Err(_e)  => {
                 println!("The uuid passed into search was not valid.");
             }
         };
-        Ok(())
+        None
+    }
+
+    fn update_asset(&mut self) {
+        match self.lookup_asset(false) {
+            Some(mut asset) => {
+                let input: String = Input::new()
+                    .with_prompt("Enter a new name for the asset")
+                    .interact_text()
+                    .expect("Something went wrong during text entry.");
+
+                asset.name = input;
+                println!("Asset updated: ");
+            },
+            None => {
+                println!("Asset not found.");
+            }
+        }
+    }
+
+    fn delete_asset(&mut self) {
+        let assets = &mut self.asset_library.dir;
+        let input: String = Input::new()
+            .with_prompt("Enter the asset's UUID")
+            .interact_text()
+            .expect("Something went wrong during text entry.");
+
+        match Uuid::parse_str(&input) {
+            Ok(uuid) => {
+                let mut index = 0;
+                while index < assets.len() {
+                    if assets[index].id == uuid {
+                        assets.remove(index);
+                    }
+                    index += 1;
+                }
+            }, 
+            Err(_e) => {
+                println!("Invalid  uuid.");
+            }
+        }
     }
 }
 
